8ce5f18149d534c4e764468f77265d41e9d6b894
Pierr Calderon Cabanillas <pierrcalderon6@hotmail.com>
2023-08-24 19:29:44 -0500

    PROYECT ARQUITECTO

---
+++ CarritoCompra/CarritoCompra.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CarritoCompra.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
